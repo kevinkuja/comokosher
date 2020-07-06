1f4ae05a27b68422b2a2c9609802a77ddb06ae5a
Kevin Kujawski <kevin.k@geopagos.com>
2020-07-06 00:04:56 -0300

    static

--- comokosher/settings.py
+++ comokosher/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/static/',
+    os.path.join(BASE_DIR, "comokosher/static"),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
